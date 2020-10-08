37af63efa519b0199dda73d17eead31b0adfafbc
wenxiaofang <15391612520@sina.cn>
2020-10-08 23:51:11 +0800

    版本发布

---
+++ docs/precache-manifest.318cf8f5d855a06e68e80be7f5247fa5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "53837dfb0e1cafc3f5f0af15180d14a1",
+    "url": "./static/media/1.53837dfb.png"
+  },
+  {
+    "revision": "77b2451f5bfb6827de8b",
+    "url": "./static/css/main.2e7776ed.chunk.css"
+  },
+  {
+    "revision": "5746e066954ddd0722465f9fe0591ecc",
+    "url": "./static/media/1.5746e066.jpg"
+  },
+  {
+    "revision": "941b3aafa130c7a3a9e1",
+    "url": "./static/js/1.941b3aaf.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "77b2451f5bfb6827de8b",
+    "url": "./static/js/main.77b2451f.chunk.js"
+  },
+  {
+    "revision": "fc8912534d723fa273b8401f6797a8bf",
+    "url": "./static/media/2.fc891253.png"
+  },
+  {
+    "revision": "5812eb02d2ee159da259ccf7336c8966",
+    "url": "./static/media/3.5812eb02.png"
+  },
+  {
+    "revision": "0847bf30d409ebf60496bfa48914d17a",
+    "url": "./static/media/4.0847bf30.png"
+  },
+  {
+    "revision": "941b3aafa130c7a3a9e1",
+    "url": "./static/css/1.ede08f39.chunk.css"
+  },
+  {
+    "revision": "e779353760dd34cee25b6f4a92bfde9a",
+    "url": "./index.html"
+  }
+];
